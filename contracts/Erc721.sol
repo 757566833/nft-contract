62bb82a2da522d54d9ed9682ac8d732404505904
zhj <757566833@qq.com>
2022-10-25 19:06:57 +0800

    feature 添加售卖功能

--- contracts/Erc721.sol
+++ contracts/Erc721.sol
@@ -12,12 +12,12 @@ contract Erc721 is ERC721URIStorage, Ownable {
     string private __symbol;
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-    
-    event OnAdd(address account, uint256 newItemId);
 
     constructor() ERC721("Gold721", "G721") {}
 
-    function mint(address account, string memory tokenId)
+    mapping(uint256 => uint256) tokenList;
+
+    function mint(address account, string memory tokenURI)
         public
         onlyOwner
         returns (uint256)
@@ -25,8 +25,7 @@ contract Erc721 is ERC721URIStorage, Ownable {
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _mint(account, newItemId);
-        _setTokenURI(newItemId, tokenId);
-        emit OnAdd(account, newItemId);
+        _setTokenURI(newItemId, tokenURI);
         return newItemId;
     }
 
@@ -41,5 +40,27 @@ contract Erc721 is ERC721URIStorage, Ownable {
     function initialize(string memory _name, string memory _symbol) public {
         __name = _name;
         __symbol = _symbol;
+    }
+
+    function sell(uint256 tokenId, uint256 price) public {
+        require(
+            ownerOf(tokenId) == msg.sender,
+            "Permission denied:nft it's not yours"
+        );
+        tokenList[tokenId] = price;
+    }
+
+    function cancelSell(uint256 tokenId, uint256 price) public {
+        require(
+            ownerOf(tokenId) == msg.sender,
+            "Permission denied:nft it's not yours"
+        );
+        tokenList[tokenId] = price;
+        delete tokenList[tokenId];
+    }
+    function buy (uint256 tokenId) payable public {
+        require(tokenList[tokenId]!=0,"item not selling");
+        require(msg.value==tokenList[tokenId],"Incorrect price");
+        safeTransferFrom(ownerOf(tokenId), msg.sender, tokenId);
     }
 }
